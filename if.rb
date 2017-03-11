c5ebb7878dc8b326063de27b905dc2ece0c6a3be
ronny2wine <ronnywine2@gmail.com>
2017-03-11 12:50:32 +0300

    getting prime numbers

--- if.rb
+++ if.rb
@@ -3,5 +3,37 @@ y=5
 
 if y>x
   puts "myname "
+
+
+
 end
 
+
+
+
+ebron= {:sport=>"basketball", :birthplace=>"Akon"}
+lebron[:high_school]="st.vincent_st.mary high school"
+lebron.keys.to_a 
+lebron.values.to_a
+
+
+
+
+
+
+class_grades = {
+  :section_one => [88, 74, 64],
+  :section_two => [99, 100]
+}
+a= class_grades.values.flatten.inject(:+)/class_grades.values.flatten.length
+
+
+
+ people = [["bob", 320], ["edgar", 152], ["maria", 125]]
+  
+  h={}
+  
+  people.each{|k,v| h}
+  p.
+  
+  
